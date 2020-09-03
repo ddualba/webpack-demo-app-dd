b638bbf4174682e663928180bbb9ba8d6f880ba0
Dar Dualba <ddualba@gmail.com>
2020-09-02 22:24:12 -0700

    Minify JS, CSS, and HTML in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,28 +1,13 @@
 const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
 		main: './src/index.js',
 		vendor: './src/vendor.js'
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/template.html'
-		}),
-		new CleanWebpackPlugin()
-	],
 	module: {
 		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader', // 3. Inject styles into DOM
-					'css-loader', // 2. Turns css into common js
-					'sass-loader' // 1. Turns sass into css
-				]
-			},
 			{
 				test: /\.html$/,
 				use: [ 'html-loader' ]
